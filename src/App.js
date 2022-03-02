3a4eb93214c7b13a9408e5a2fbc4734b0001d9ee
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2022-03-02 20:33:49 +0900

    pc only

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import { useEffect, useRef } from "react";
+import styled from "styled-components";
 import About from "./About";
 import Contact from "./Contact";
 import Header from "./Header";
 import Main from "./Main";
 import Skills from "./Skills";
 import Works from "./Works";
+
+const PcOnly = styled.div`
+  display: none;
+
+  @media screen and (max-width: 599px) {
+    display: block;
+    height: 100vh;
+    width: 100vw;
+    position: fixed;
+    background-color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 100;
+  }
+`;
 
 function App() {
   const aboutRef = useRef();
@@ -23,6 +40,10 @@ function App() {
   }, []); // 새로고침시 뷰포인트 맨위로 이동
   return (
     <>
+      <PcOnly>
+        죄송합니다 현재 모바일 버전은 준비중입니다. pc버전으로 이용해주시면
+        감사하겠습니다. ㅠㅠ
+      </PcOnly>
       <Header />
       <Main scrollTo={scrollTo} />
       <div ref={aboutRef}>
